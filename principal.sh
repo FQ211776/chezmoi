12ffdb5be2eac89a6d83ac8573ca6a0f3c562686
Roberto Flores <fq211776@alumno.udb.edu.sv>
2024-06-26 01:23:06 -0600

    agregado script logo

--- principal.sh
+++ principal.sh
@@ -6,9 +6,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Directorio que contiene los scripts a llamar (relativo al script actual)
 SCRIPTS_DIR="$SCRIPT_DIR/scripts"
 
-# # Directorio que contiene los scripts a llamar
-# SCRIPTS_DIR="./scripts"
-
 # Función para cargar un script y sus funciones
 cargar_script() {
     local script_path="$SCRIPTS_DIR/$1"
@@ -23,6 +20,10 @@ cargar_script() {
 
 # Cargar scripts necesarios (ajusta los nombres según tus scripts)
 cargar_script "utilidades.sh"
+cargar_script "logo.sh"
+
+# inicio del script
+logos
 
 # Ejemplo de uso de funciones de los scripts cargados
 imprimir_mensaje "Hola desde el script principal"
